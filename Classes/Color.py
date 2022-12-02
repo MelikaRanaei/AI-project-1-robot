00bec784f9ee52c7b6e11a66269897032e767958
Mohammad Baghbanzadeh <mohammad.baghbanzadeh.cs.fum@gmail.com>
2022-12-02 07:15:48 +0330

    adding Colors Class

---
+++ Classes/Color.py
@@ -0,0 +1,13 @@
+class Colors:
+    WARNING = '\033[93m'
+    white = '\033[97m'
+    black = '\033[90m'
+    reset = '\033[0m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
