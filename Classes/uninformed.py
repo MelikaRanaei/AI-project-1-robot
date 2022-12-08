23eaad104caa88cd3a443b36e1091c42ec18f276
MelikaRanaei <94311875+MelikaRanaei@users.noreply.github.com>
2022-12-08 11:22:04 +0330

    add bfs

--- Classes/uninformed.py
+++ Classes/uninformed.py
@@ -1,8 +1,24 @@
-from Classes.Node import Node
+from Classes.Node import Node , is_goal_node , succesor
 from Classes.Table import Table
+ 
+        
+def bfs(table:Table):
+    node=Node(table.robot,table.butter)
+    if is_goal_node(node,table.goals):
+        return node
+    
+    queue=[node]
+    
+    while queue:
+        node = queue.pop(0)
+        for child in succesor(node , table):
+            if is_goal_node(child,table.goals):
+                return child
+            else:
+                queue.extend(succesor(child , table))
+                
+    return -1    
 
-def bfs(current_node: 'Node'):
-    pass
 
 
 def dfs():
