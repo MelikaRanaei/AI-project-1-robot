3089cfe4ddc87374f662517761f535092524b3f5
MelikaRanaei <94311875+MelikaRanaei@users.noreply.github.com>
2022-12-08 18:41:02 +0330

    Add DFS

--- Classes/uninformed.py
+++ Classes/uninformed.py
@@ -1,30 +1,58 @@
-from Classes.Node import Node , is_goal_node , succesor
+from Classes.Node import Node 
+from Classes.Node import is_goal_node , succesor
 from Classes.Table import Table
  
         
-
-def bfs(table:Table):
+def BFS(table:Table):
+    
     node=Node(table.robot,table.butter)
-    if is_goal_node(node,table.goals):
-        return node
     
     queue=[node]
     
     while queue:
+     
         node = queue.pop(0)
-        for child in successor(node , table):
-            if is_goal_node(child,table.goals):
-                return child
-            else:
-                if child.is_visited==False:
-                    queue.extend(successor(child , table))
+
+        if Node.is_goal_node(node,table.goals):
+        
+            return node
+#             return node.find_path_from_source()
+        else:
+            node.is_visited=True
+            
+        Node.successor(node , table) 
+        
+        queue.extend(node.children_node)
+         
+    return -1           
                 
-    return -1  
+                
+            
+def DFS(table:Table):
+    
+    node = Node(table.robot,table.butter)
 
-
-
-def dfs():
-    pass
+    stack=[node]
+    
+    while stack:
+     
+        node = stack.pop()
+        
+        if Node.is_goal_node(node,table.goals):
+         
+            return node
+#             return node.find_path_from_source()
+        
+        else:
+            node.is_visited=True
+            
+        Node.successor(node , table)
+        
+        stack.extend(node.children_node[::-1])
+         
+    return -1 
+   
+   
 
 def ucs():
     pass
