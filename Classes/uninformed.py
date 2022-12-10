ee9204b65168bb19ca8dd5f2888ee6b7c38677f1
ShervinEmrani <emranishervin@gmail.com>
2022-12-10 11:16:27 +0330

    ucs changed

--- Classes/uninformed.py
+++ Classes/uninformed.py
@@ -1,4 +1,4 @@
-# from asyncio import  Queue
+from asyncio import Queue, PriorityQueue
 from multiprocessing import Queue
 from Classes.Node import Node
 from Classes.Table import Table
@@ -52,28 +52,29 @@ def DFS(table:Table):
         
         stack.extend(node.children_node[::-1])
 
-    return -1 
-   
+    return -1
 
-
-def ucs(initial_node: Node, table: Table):
-    ucs_Q = Queue()
+def ucs(initial_node: Node):
+    ucs_Q = PriorityQueue()
     ucs_Q.put(initial_node)
     visited = []
     while not ucs_Q.empty():
         popped_node = ucs_Q.get()
-        visited.append(popped_node)
+        if popped_node.is_goal_node():
+            return popped_node.last_move
+        for i in range(4):
+            child = popped_node.successor(i)
+            ucs_Q.put(popped_node.g_n, child)
 
-        print(popped_node.robot)
-        popedlist = popped_node.successor(popped_node, table)
-
-        for child in popedlist:
-            # if child[0][0].is_visited:
-            #     continue
-            if child[0] not in visited:
-                ucs_Q.put(child[0])
-                # total_cost = cost + tree.get_cost(child, child[0])
-                print(child)
+        # popedlist = popped_node.successor(popped_node)
+        #
+        # for child in popedlist:
+        #     # if child[0][0].is_visited:
+        #     #     continue
+        #     if child[0] not in visited:
+        #         ucs_Q.put(child[0])
+        #         # total_cost = cost + tree.get_cost(child, child[0])
+        #         print(child)
 
 
 def ids():
